3fd73b4a3e6a14c0bb7ffa2af486704886de6abd
Nataly Arkadeva <natatenok73@mail.ru>
2021-12-22 17:28:56 +0400

    add day 5

--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,30 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike bike = new Motorbike("Honda", "yellow", 2005);
+        System.out.println(bike.getModel() + " " + bike.getColor() + " " + bike.getYear());
+    }
+}
+class Motorbike {
+    private String model;
+    private String color;
+    private int year;
 
+    public Motorbike(String model, String color, int year){
+        this.model = model;
+        this.color = color;
+        this.year = year;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getYear() {
+        return year;
     }
 }
