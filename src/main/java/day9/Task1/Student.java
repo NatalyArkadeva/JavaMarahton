3c1afb8e27f17229fd5157f4aaed770e082f4483
Nataly Arkadeva <natatenok73@mail.ru>
2021-12-27 21:29:07 +0400

    complete day 9 tasks

---
+++ src/main/java/day9/Task1/Student.java
@@ -0,0 +1,22 @@
+package day9.Task1;
+
+public class Student extends Human {
+    private String groupName;
+
+    public String getGroupName() {
+        return groupName;
+    }
+
+    public Student(String name, String groupName) {
+        super(name);
+        this.groupName = groupName;
+    }
+
+    @Override
+    public void printInfo() {
+        super.printInfo();
+        System.out.printf("Этот студент с именем %s\n", getName());
+    }
+
+
+}
--- src/main/java/day9/Task1/Task1.java
+++ src/main/java/day9/Task1/Task1.java
@@ -2,6 +2,13 @@ package day9.Task1;
 
 public class Task1 {
     public static void main(String[] args) {
+        Student student = new Student("Tom", "3a");
+        Teacher teacher = new Teacher("Bob", "Math");
+
+        System.out.println(student.getGroupName());
+        System.out.println(teacher.getSubjectName());
+        student.printInfo();
+        teacher.printInfo();
 
     }
 }
