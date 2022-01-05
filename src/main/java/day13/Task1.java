32ce81bc206e15f757b3fe1cd3a07e675d2117e2
Nataly Arkadeva <natatenok73@mail.ru>
2022-01-05 12:04:47 +0400

    complete day 12

--- src/main/java/day13/Task1.java
+++ src/main/java/day13/Task1.java
@@ -2,6 +2,25 @@ package day13;
 
 public class Task1 {
     public static void main(String[] args) {
+        User user1 = new User("Петя");
+        User user2 = new User("Вася");
+        User user3 = new User("Федя");
+
+        user1.sendMessage(user2, "Hello " + user2.getName() + "!");
+        user1.sendMessage(user2, "How are you?");
+        user2.sendMessage(user1, "Hello " + user1.getName() + "!");
+        user2.sendMessage(user1, "Fine!");
+        user2.sendMessage(user1, "123");
+        user3.sendMessage(user1, "i am user 3");
+        user3.sendMessage(user1, "i am user 3");
+        user3.sendMessage(user1, "i am user 3");
+        user1.sendMessage(user3, "Hello " + user3.getName() + "!");
+        user1.sendMessage(user3, "Hello " + user3.getName() + "!");
+        user1.sendMessage(user3, "Hello " + user3.getName() + "!");
+        user3.sendMessage(user1, "i am user 3");
+
+        MessageDatabase.showDialog(user1, user2);
+        System.out.println(new Message(user1, user2, "123"));
 
     }
 }
