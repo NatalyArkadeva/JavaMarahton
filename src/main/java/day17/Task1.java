b0564e2345350e777f5dd83d6dfbbc545e8cc776
Nataly Arkadeva <natatenok73@mail.ru>
2022-01-12 14:56:24 +0400

    complete day 17

--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,13 @@
 package day17;
 
+
 public class Task1 {
     public static void main(String[] args) {
+        ChessPiece[] chessPiece = new ChessPiece[8];
+        for (int i = 0; i < chessPiece.length; i++) {
+            chessPiece[i] = (i < 4) ? ChessPiece.PAWN_WHITE : ChessPiece.ROOK_BLACK;
+            System.out.print(chessPiece[i].getDesignation() + " ");
+        }
 
     }
 }
--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -2,6 +2,26 @@ package day17;
 
 public class Task2 {
     public static void main(String[] args) {
+        ChessPiece[][] pieces = new ChessPiece[8][8];
+        for (int i = 0; i < pieces.length; i++) {
+            for (int j = 0; j < pieces[i].length; j++) {
 
+                pieces[i][j] = (i == 0 && (j == 0 || j == 5)) ? ChessPiece.ROOK_BLACK :
+                        (i == 0 && j == 6) ? ChessPiece.KING_BLACK :
+                                ((i == 1 && j == 1) || (i == 7 && j == 5)) ? ChessPiece.ROOK_WHITE :
+                                        ((i == 1 & (j == 4 || j == 5 || j == 7)) || (i == 2 && (j == 0 || j == 6))) ? ChessPiece.PAWN_BLACK :
+                                                (i == 2 && j == 2) ? ChessPiece.KNIGHT_BLACK :
+                                                        (i == 3 && j == 0) ? ChessPiece.QUEEN_BLACK :
+                                                                ((i == 3 && j == 3) || (i == 5 && j == 4)) ? ChessPiece.BISHOP_WHITE :
+                                                                        (i == 4 && j == 3) ? ChessPiece.BISHOP_BLACK :
+                                                                                ((i == 4 && j == 4) || (i == 5 && j == 5) || (i == 6 && (j == 0 || j == 5 || j == 7))) ? ChessPiece.PAWN_WHITE :
+                                                                                        (i == 6 && j == 3) ? ChessPiece.QUEEN_WHITE :
+                                                                                                (i == 7 && j == 6) ? ChessPiece.KING_WHITE : ChessPiece.EMPTY;
+
+            }
+        }
+
+        ChessBoard board = new ChessBoard(pieces);
+        board.print(pieces);
     }
 }
