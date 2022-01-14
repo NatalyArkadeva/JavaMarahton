71d41286dbecf9b7a58a01e192453a8bf746027c
Nataly Arkadeva <natatenok73@mail.ru>
2022-01-14 18:50:58 +0400

    correct day 17

--- src/main/java/day17/Task2.java
+++ src/main/java/day17/Task2.java
@@ -5,21 +5,20 @@ public class Task2 {
         ChessPiece[][] pieces = new ChessPiece[8][8];
         for (int i = 0; i < pieces.length; i++) {
             for (int j = 0; j < pieces[i].length; j++) {
-
-                pieces[i][j] = (i == 0 && (j == 0 || j == 5)) ? ChessPiece.ROOK_BLACK :
-                        (i == 0 && j == 6) ? ChessPiece.KING_BLACK :
-                                ((i == 1 && j == 1) || (i == 7 && j == 5)) ? ChessPiece.ROOK_WHITE :
-                                        ((i == 1 & (j == 4 || j == 5 || j == 7)) || (i == 2 && (j == 0 || j == 6))) ? ChessPiece.PAWN_BLACK :
-                                                (i == 2 && j == 2) ? ChessPiece.KNIGHT_BLACK :
-                                                        (i == 3 && j == 0) ? ChessPiece.QUEEN_BLACK :
-                                                                ((i == 3 && j == 3) || (i == 5 && j == 4)) ? ChessPiece.BISHOP_WHITE :
-                                                                        (i == 4 && j == 3) ? ChessPiece.BISHOP_BLACK :
-                                                                                ((i == 4 && j == 4) || (i == 5 && j == 5) || (i == 6 && (j == 0 || j == 5 || j == 7))) ? ChessPiece.PAWN_WHITE :
-                                                                                        (i == 6 && j == 3) ? ChessPiece.QUEEN_WHITE :
-                                                                                                (i == 7 && j == 6) ? ChessPiece.KING_WHITE : ChessPiece.EMPTY;
-
+                pieces[i][j] = ChessPiece.EMPTY;
             }
         }
+        pieces[0][0] = pieces[0][5] = ChessPiece.ROOK_BLACK;
+        pieces[0][6] = ChessPiece.KING_BLACK;
+        pieces[1][1] = pieces[7][5] = ChessPiece.ROOK_WHITE;
+        pieces[1][4] = pieces[1][5] = pieces[1][7] = pieces[2][0] = pieces[2][6] = ChessPiece.PAWN_BLACK;
+        pieces[2][2] = ChessPiece.KNIGHT_BLACK;
+        pieces[3][0] = ChessPiece.QUEEN_BLACK;
+        pieces[3][3] = pieces[5][4] = ChessPiece.BISHOP_WHITE;
+        pieces[4][3] = ChessPiece.BISHOP_BLACK;
+        pieces[4][4] = pieces[5][5] = pieces[6][0] = pieces[6][5] = pieces[6][7] = ChessPiece.PAWN_WHITE;
+        pieces[6][3] = ChessPiece.QUEEN_WHITE;
+        pieces[7][6] = ChessPiece.KING_WHITE;
 
         ChessBoard board = new ChessBoard(pieces);
         board.print(pieces);
