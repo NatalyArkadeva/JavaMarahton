45ca9b8b1c9c828781b8788bc7e257906bc2fbb3
Nataly Arkadeva <natatenok73@mail.ru>
2022-01-20 18:08:47 +0400

    complete day 19

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,54 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
     public static void main(String[] args) {
+        File file = new File("src/main/resources/dushi.txt");
 
+        try {
+            Scanner scanner = new Scanner(file);
+            scanner.useDelimiter("[»«<>…N„“.,:;()?!\"\\s–+]");
+
+            Map<String, Integer> wordAndCount = new HashMap<>();
+
+            List<String> wordsDushi = new ArrayList<>();
+            while (scanner.hasNext()) {
+                wordsDushi.add(scanner.next());
+            }
+            scanner.close();
+
+            for (String w : wordsDushi) {
+                if (wordAndCount.containsKey(w)) {
+                    wordAndCount.put(w, wordAndCount.get(w) == null ? 1 : wordAndCount.get(w) + 1);
+                } else if (!w.isEmpty()) {
+                    wordAndCount.put(w, 1);
+                }
+            }
+            System.out.println(wordAndCount);
+            System.out.println("Слово 'Чичиков' повторяется " + wordAndCount.get("Чичиков") + " раз.");
+
+            findMostUsedWords(wordAndCount);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+    }
+
+    public static void findMostUsedWords(Map<String, Integer> map) {
+        for (int count = 0; count < 101; count++) {
+            int maxValueInMap = Collections.max(map.values());
+            for (Map.Entry<String, Integer> entry : map.entrySet()) {
+                if (entry.getValue() == maxValueInMap) {
+                    System.out.println(entry.getKey() + " : " + entry.getValue());
+                    map.put(entry.getKey(), 0);
+                }
+            }
+        }
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,11 +1,69 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("src/main/resources/taxi_cars.txt");
+        Map<Integer, Point> taxiLocation = new HashMap<>();
+        makeMapIndexAndTaxiLocation(file, taxiLocation);
+        searchAllCarsInTheSquare(taxiLocation);
 
     }
+
+    public static void makeMapIndexAndTaxiLocation(File file, Map map) {
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] cars = line.split(" ");
+                if (cars.length != 3) {
+                    throw new IllegalArgumentException();
+                }
+                for (int i = 0; i < cars.length; i++) {
+                    map.put(Integer.parseInt(cars[0]), new Point(Integer.parseInt(cars[1]), Integer.parseInt(cars[2])));
+                }
+            }
+            scanner.close();
+            System.out.println(map);
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Некорректный файл");
+        }
+
+    }
+
+    public static void searchAllCarsInTheSquare(Map<Integer, Point> map) {
+        Scanner scanner = new Scanner(System.in);
+        int x1 = -1;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = -2;
+        while (Math.abs(x1 - x2) != (Math.abs(y1 - y2))) {
+            System.out.println("Введите координаты диагонали квадрата(x1, y1, x2, y2): 4 целых числа в диапазоне от 0 до 99");
+            x1 = scanner.nextInt();
+            y1 = scanner.nextInt();
+            x2 = scanner.nextInt();
+            y2 = scanner.nextInt();
+        }
+        scanner.close();
+
+        int count = 0;
+        for (Map.Entry<Integer, Point> entry : map.entrySet()) {
+            if (entry.getValue().getX() > x1 && entry.getValue().getX() < x2 && entry.getValue().getY() > y1 && entry.getValue().getY() < y2) {
+                System.out.println(entry.getKey());
+                count++;
+            }
+        }
+
+        System.out.println("Общее количество машин в квадрате: " + count);
+    }
+
 }
 
